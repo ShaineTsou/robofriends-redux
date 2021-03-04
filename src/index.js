02e2bb5b87fd89cf41b5614c4b9176c3d2570ca3
Shaine Tsou <shaineagro06@gmail.com>
2021-03-04 20:36:18 +0800

    provide a valid apple-touch-icon and a maskable icon

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { createLogger } from 'redux-logger';
 import 'tachyons';
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
 import { requestRobots, searchRobots } from './reducers'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './index.css';
 
@@ -24,4 +24,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
