139b15119fba37b6c7eedf00660dd38085b135d7
Shaine Tsou <shaineagro06@gmail.com>
2021-03-04 21:07:00 +0800

    add label for input and decription of the page

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../containers/App.css';
 
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
+      <div>
+        <label for="searchBox">↓  Search Me  ↓</label>
+      </div>
       <input
+        id='searchBox'
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
