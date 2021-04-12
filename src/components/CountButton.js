cda4da7d2e346b1de5f93302baab951b526b22e4
Shaine Tsou <shaineagro06@gmail.com>
2021-04-12 21:23:44 +0800

    make it prettier

--- src/components/CountButton.js
+++ src/components/CountButton.js
@@ -1,26 +1,26 @@
 // This file is used to play around with the Pre-commit Hook of Prettier
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CountButton extends Component {
-    constructor(              ) {
-        super(    );
-        this.state = {          count: 0       }
-    }
-    
-    incrementByOne = () => {
-        this.setState(prevState => ({            count: prevState.count + 1
-        }));
-    }
+  constructor() {
+    super();
+    this.state = { count: 0 };
+  }
 
-    render() {        return (
-            <div>
-                <button onClick={
-                    this.incrementByOne} >
-                        {`Count: ${this.state.count}`}</button>
-            </div>
-        )
-    }
+  incrementByOne = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.incrementByOne}>
+          {`Count: ${this.state.count}`}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default CountButton;
